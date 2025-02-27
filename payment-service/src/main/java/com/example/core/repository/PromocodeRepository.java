40105e9809450cd5781d975f27b85cecbe2cd123
MariaShakuro <mashashakuro25@gmail.com>
2025-02-27 21:53:36 +0300

    some fix in payment-service

--- payment-service/src/main/java/com/example/core/repository/PromocodeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.core.repository;
-
-import com.example.core.entity.Promocode;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PromocodeRepository extends JpaRepository<Promocode, Long> {
-    Promocode findByCode(String code);
-}
